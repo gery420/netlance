7d04bbdb0c4fb75f64c873eb9ed2142c6853d2ba
Ashish <mashishkr025@gmail.com>
2025-07-24 00:07:43 +0530

    fixes in cors

--- server/index.js
+++ server/index.js
@@ -12,11 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    allowedOrigins: ["https://netlance.vercel.app", "http://localhost:3000"],
+    allowedOrigins: ["https://netlance.vercel.app", "http://localhost:3000","https://netlance-dkay.vercel.app/"],
     AccessControlAllowOrigin: true,
-    origin: ["https://netlance.vercel.app", "http://localhost:3000"],
+    origin: ["https://netlance.vercel.app", "http://localhost:3000", "https://netlance-dkay.vercel.app/"],
     exposedHeaders: ["Content-Length", "X-Requested-With"],
-
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
