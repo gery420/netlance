ddcfc559a226fd5856bad2d6f6298b555a3c4c02
Ashish <mashishkr025@gmail.com>
2025-07-31 02:31:45 +0530

    fixs

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {RegisterSeller, Dashboard} = require("../controllers/seller");
 const router = express.Router();
 const upload = require("../middleware/upload");
-const Auth = require("../middleware/auth");
+const Auth = require("../middleware/Auth");
 
 router.post('/registerSeller', upload.fields([{ name: 'profilePicture', maxCount: 1 }]), RegisterSeller);
 router.get('/dashboard', Auth, Dashboard);
