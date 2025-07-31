be94e774f9101c7d91fc7ea739db8fbab67f06c4
Ashish <mashishkr025@gmail.com>
2025-07-31 23:16:36 +0530

    f

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,6 +12,6 @@ router.post("/forgotPassword", GeneratePasswordResetToken);
 router.post("/verifyOtp", VerifyOtp);
 router.post("/setPassword", SetPassword);
 router.post("/resetPassword", Auth, ResetPassword);
-router.post("/updateProfile", upload.single("profilePicture"), Auth, UpdateProfile);
+router.post("/updateProfile", upload.fields([{ name: "profilePicture", maxCount: 1 }]), Auth, UpdateProfile);
 
 module.exports = router;
